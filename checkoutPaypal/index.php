9cd54cb35b8c5202acd5f24409a782fcaa1407e4
TWZedryd <monserratriverariv21@gmail.com>
2025-03-21 12:46:28 -0600

    checkout Paypal

---
+++ checkoutPaypal/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title> PayPal Checkout </title>
+</head>
+
+<body>
+
+    <div id="paypal-button-container"></div>
+
+
+    <script src="https://www.paypal.com/sdk/js?client-id=test&currency=MXN"></script>
+
+    <script src="paypal.js"> </script>
+</body>
+
+</html>
+    
